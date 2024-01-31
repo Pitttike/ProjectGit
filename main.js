6e23bade2fe1e2c0bbe59d8091a62a2b762c295f
Petrik Diak <diak@petrik.hu>
2024-01-31 13:27:13 +0100

    Szám hozzáadás listához

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import './style.css'
+
+let numberList = [];
+
+document.getElementById("addNumber").addEventListener("click", () => {
+  numberList.push(document.getElementById("numberInput").value)
+})

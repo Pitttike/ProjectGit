74a6e3e983238463e52737a9eedcae3afaeb1016
Petrik Diak <diak@petrik.hu>
2024-01-31 13:40:26 +0100

    Minimum kiválasztása

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ document.getElementById("addNumber").addEventListener("click", () => {
   numberList.push(document.getElementById("numberInput").value)
   document.getElementById("numberListHtml").innerHTML = numberList.join(', ')
   });
+
+  document.getElementById("selectMinimum").addEventListener("click", () => {
+    let minimum = 9999999999
+    numberList.forEach(element => {
+        if (element<minimum) {
+          minimum = element
+        }
+    });
+    document.getElementById("numberListHtml").innerHTML = minimum;
+  })
